247627408f4bb0bb2ab8ff57b2e5a631bb74a5e8
VikRudkovskaya <vik-rudkovskaya@yandex.ru>
2016-02-19 16:32:42 +0300

    Remove DerivedData

--- DerivedData/BarcodeReaderEAN-13/Build/Intermediates/BarcodeReaderEAN-13.build/Debug-iphonesimulator/BarcodeReaderEAN-13.build/Objects-normal/x86_64/ViewController.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/viktoriarudkovskaya/Desktop/Projects/BarcodeReaderEAN-13/BarcodeReaderEAN-13/ViewController.m \
-  /Users/viktoriarudkovskaya/Desktop/Projects/BarcodeReaderEAN-13/BarcodeReaderEAN-13/ViewController.h \
-  /Users/viktoriarudkovskaya/Desktop/Projects/BarcodeReaderEAN-13/BarcodeReaderEAN-13/model/BarcodeParserImpl.h \
-  /Users/viktoriarudkovskaya/Desktop/Projects/BarcodeReaderEAN-13/BarcodeReaderEAN-13/model/BarcodeParser.h
--- DerivedData/BarcodeReaderEAN-13/Build/Intermediates/BarcodeReaderEAN-13.build/Debug-iphonesimulator/BarcodeReaderEAN-13.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/viktoriarudkovskaya/Desktop/Projects/BarcodeReaderEAN-13/BarcodeReaderEAN-13/main.m \
-  /Users/viktoriarudkovskaya/Desktop/Projects/BarcodeReaderEAN-13/BarcodeReaderEAN-13/AppDelegate.h
